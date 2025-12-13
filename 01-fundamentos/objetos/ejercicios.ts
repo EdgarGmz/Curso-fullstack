50cc95d93ab3e1574be5a9ccdd449dc7f0f0601f
EdgarGmz <66767346+EdgarGmz@users.noreply.github.com>
2025-12-13 14:18:10 -0600

    Se agregan mas ejercicios de objetos

--- 01-fundamentos/objetos/ejercicios.ts
+++ 01-fundamentos/objetos/ejercicios.ts
@@ -1,7 +1,8 @@
 // EJERCICIOS DE OBJETOS EN TYPESCRIPT
 
 // EJERCICIO 1
-// Crea un objeto llamado 'auto' con las propiedades: marca (string), modelo (string), año (number) y encendido (boolean). Imprime el objeto en consola.
+// Crea un objeto llamado 'auto' con las propiedades: marca (string), modelo 
+// (string), año (number) y encendido (boolean). Imprime el objeto en consola.
 const auto ={
     marca: "KIA",
     modelo: "Forte",
@@ -12,7 +13,8 @@ const auto ={
 console.log(`La marca del carro es: ${auto.marca}\nEl modelo es: ${auto.modelo}\nEl año del auto es: ${auto.anio} y \n${auto.encendido ? 'esta encendido' : 'esta apagado'}`)
 
 // EJERCICIO 2
-// Agrega un método al objeto 'auto' llamado 'encender' que cambie la propiedad 'encendido' a true y muestre un mensaje en consola.
+// Agrega un método al objeto 'auto' llamado 'encender' que cambie la propiedad 
+// 'encendido' a true y muestre un mensaje en consola.
 let miAuto = {
     modelo: "KIA",
     arrancar: function(){
@@ -22,8 +24,10 @@ let miAuto = {
 
 console.log(`Voy a encender mi auto ${miAuto.modelo}`)
 miAuto.arrancar()
+
 // EJERCICIO 3
-// Crea una función que reciba un objeto con las propiedades 'nombre' (string) y 'edad' (number) y retorne un string con la información formateada.
+// Crea una función que reciba un objeto con las propiedades 'nombre' 
+// (string) y 'edad' (number) y retorne un string con la información formateada.
 type person = {
     name: string,
     age: number,
@@ -49,23 +53,50 @@ function showInfo(c:person, b: person){
 showInfo(client, boss)
 
 // EJERCICIO 4
-// Crea un array de objetos 'alumnos', cada uno con las propiedades 'nombre', 'matricula' y 'activo'. Escribe una función que retorne solo los alumnos activos.
-type alumn = {
-    matricule: number
-    isActive: boolean
+// Crea un array de objetos 'alumnos', cada uno con las propiedades 
+// 'nombre', 'matricula' y 'activo'. Escribe una función que retorne solo los alumnos activos.
+
+interface alumnos { nombre: string, matricula: number, activo: boolean }
+
+const a1 : alumnos = {
+    nombre: 'Edgar Gmz',
+    matricula: 22038,
+    activo: true
 }
 
-const p1: person = {
-    name: "Edgar",
-    age: 35,
-    isAlive: true
+const a2 : alumnos = {
+    nombre: 'Jimenez',
+    matricula: 22039,
+    activo: false
 }
 
-const a1 : alumn = {
-    matricule: 22038
+
+const a3 : alumnos = {
+    nombre: 'Octavio',
+    matricula: 22040,
+    activo: true
 }
+
+
+const a4 : alumnos = {
+    nombre: 'Alma',
+    matricula: 22041,
+    activo: false
+}
+
+let listaAlumnos : alumnos [] = [a1, a2, a3, a4]
+
+function showInfoAlumnos(lista: alumnos[]){
+    lista.map(alumno =>{
+        console.log(`\nEl nombre del alumno es ${alumno.nombre}\nsu matricula es ${alumno.matricula}\ny ${alumno.activo ? 'es vigente' : 'es baja'}`)
+    })
+}
+
+showInfoAlumnos(listaAlumnos)
+
 
 
 
 // EJERCICIO 5
-// Define un type alias para representar un objeto 'Libro' con propiedades: título, autor, año y prestado (boolean). Crea una función que reciba un libro y cambie su estado de 'prestado'.
+// Define un type alias para representar un objeto 'Libro' con propiedades: 
+// título, autor, año y prestado (boolean). Crea una función que reciba un libro y cambie su estado de 'prestado'.
